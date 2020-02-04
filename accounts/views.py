47ca1bd04a10f14e99c5325ec24d198bc3a5ae04
Tahmid <tahmidrana@gmail.com>
2020-02-04 17:52:31 +0600

    add signin & signup page

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
